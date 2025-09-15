aef726df030ad73bb967f65595ccf3c17cd18c64
qualquercois1 <cassiodossantosrodrigues@gmail.com>
2025-09-15 15:14:07 -0300

    organizando o projeto

---
+++ backend/src/main/java/br/com/qualquercois1/gamble/GambleApplication.java
@@ -0,0 +1,13 @@
+package br.com.qualquercois1.gamble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GambleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GambleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/qualquercois1/gamble/GambleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qualquercois1.gamble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GambleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
